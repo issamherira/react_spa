3dd6b89ac018d5670a76661dc4fa1896983dbb4e
osmano01 <issam.herira@sesame.com.tn>
2021-01-20 11:37:19 +0100

    Create a React app and publish it to GitHub Pages

---
+++ src/Contact.js
@@ -0,0 +1,45 @@
+import React,{ useState } from 'react'
+import MapGL, {GeolocateControl } from 'react-map-gl'
+
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+const TOKEN='pk.eyJ1IjoibGVqcmkiLCJhIjoiY2trNGt4N3RiMTQ5dDMycW84aTExNmw2MiJ9.SMqyvt0xiVHcKvZhWBAlFg'
+
+const geolocateStyle = {
+    float: 'left',
+    margin: '50px',
+    padding: '10px'
+};
+
+const Contact = () => {
+
+    const [viewport, setViewPort ] = useState({
+        width: "100%",
+        height: 900,
+        latitude: 0,
+        longitude: 0,
+        zoom: 2
+    })
+
+    const _onViewportChange = viewport => setViewPort({...viewport, transitionDuration: 3000 })
+
+    return (
+        <div style={{ margin: '0 auto'}}>
+
+            <MapGL
+                {...viewport}
+                mapboxApiAccessToken={TOKEN}
+                mapStyle="mapbox://styles/mapbox/streets-v11"
+                onViewportChange={_onViewportChange}
+            >
+                <GeolocateControl
+                    style={geolocateStyle}
+                    positionOptions={{enableHighAccuracy: true}}
+                    trackUserLocation={true}
+                />
+            </MapGL>
+        </div>
+    )
+}
+
+export default Contact
